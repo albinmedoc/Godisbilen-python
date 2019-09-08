678652c080ffc619e217b6897a4dcab08aec7613
albinmedoc <stamline203@gmail.com>
2019-09-08 23:05:43 +0200

    Deleted Admin Table and Admin Login Form

--- godisbilen/admin/admin.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
-from godisbilen.app import db
-
-class Admin(db.Model):
-    __tablename__ = "admin"
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True, nullable=False)
-    password = Column(String(60), nullable=False)
