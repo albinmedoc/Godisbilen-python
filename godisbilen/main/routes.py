d7adbb4bb5cb9eb85e64a5ee17570c20cbff5d1f
albinmedoc <stamline203@gmail.com>
2019-09-10 19:12:41 +0200

    Made Terms of Service page

--- godisbilen/main/routes.py
+++ godisbilen/main/routes.py
@@ -5,3 +5,7 @@ bp_main = Blueprint("main", __name__)
 @bp_main.route("/")
 def home():
     return render_template("home.html")
+
+@bp_main.route("/tos")   
+def tos():
+    return render_template("tos.html")
