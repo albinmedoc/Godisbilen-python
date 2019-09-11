0cf0b6771adc09a63b5d2c8c5505738c65b0dd62
albinmedoc <stamline203@gmail.com>
2019-09-11 18:14:24 +0200

    Made pages for contact and about us

--- godisbilen/main/routes.py
+++ godisbilen/main/routes.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template
+from .utils import shop_open
 
 bp_main = Blueprint("main", __name__)
 
 @bp_main.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", shop_open=shop_open())
 
 @bp_main.route("/working_areas")   
 def working_areas():
@@ -13,3 +14,11 @@ def working_areas():
 @bp_main.route("/tos")   
 def tos():
     return render_template("tos.html")
+
+@bp_main.route("/contact")   
+def contact():
+    return render_template("contact.html")
+
+@bp_main.route("/about")   
+def about():
+    return render_template("about.html")
