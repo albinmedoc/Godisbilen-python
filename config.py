cce72bd1be814ab55f00022b38f5c0fff774ff75
albinmedoc <stamline203@gmail.com>
2019-09-11 15:47:08 +0200

    added opening hours

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import time
 from dotenv import load_dotenv
 
 class Config(object):
@@ -14,6 +15,12 @@ class Config(object):
 
     BASIC_AUTH_USERNAME = "basic"
     BASIC_AUTH_PASSWORD = "basic"
+
+    OPENING_HOURS = {
+        "4": [[time(20), time(23, 59, 59, 59)]],
+        "5": [[time(0), time(4)], [time(20), time(23, 59, 59, 59)]],
+        "6": [[time(0), time(4)], [time(20), time(23, 59, 59, 59)]]
+    }
 
     #Database
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
