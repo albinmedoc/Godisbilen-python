7509f0ffc23261e1d86e4c3a8565a2dc5fe059f6
albinmedoc <stamline203@gmail.com>
2019-09-16 21:17:09 +0200

    showing estimated_timeand made phase and order_nmr searchable

--- godisbilen/order/views.py
+++ godisbilen/order/views.py
@@ -1,8 +1,9 @@
 from godisbilen.admin.views import AdminView
 
 class OrderView(AdminView):
+    column_searchable_list = ["order_number", "phase"]
     page_size = 50
     can_view_details = True
-    column_searchable_list = ["order_number", "location_id"]
     create_modal = True
     edit_modal = True
+    column_list = ("order_number", "user", "location", "placed", "estimated_time", "phase")
