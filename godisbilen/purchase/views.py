47bcbe48fc971c14ed34293a6afda88cf2fe0db3
albinmedoc <stamline203@gmail.com>
2019-09-16 21:21:08 +0200

    Showing user, count_products and products

--- godisbilen/purchase/views.py
+++ godisbilen/purchase/views.py
@@ -1,7 +1,13 @@
 from godisbilen.admin.views import AdminView
+from godisbilen.purchase import Purchase
+from godisbilen.user import User
 
 class PurchaseView(AdminView):
+    column_searchable_list = [User.phone_number]
     page_size = 50
     can_view_details = True
     create_modal = True
     edit_modal = True
+    column_hide_backrefs = False
+    column_select_related_list = (Purchase.user, Purchase.products)
+    column_list = ("id", "user", "count_products", "products")
