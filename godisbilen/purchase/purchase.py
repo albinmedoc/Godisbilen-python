1a0af1af34754717c0cb0ff44cc104fc695f186f
albinmedoc <stamline203@gmail.com>
2019-09-16 21:21:32 +0200

    Added property for count_products

--- godisbilen/purchase/purchase.py
+++ godisbilen/purchase/purchase.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.hybrid import hybrid_property
 from godisbilen.app import db
 
 purchase_products = db.Table("purchase_products",
@@ -12,4 +13,12 @@ class Purchase(db.Model):
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("person.id"))
     user = relationship("User", back_populates="purchases")
-    products = relationship("Product", secondary=purchase_products, back_populates="purchases")
+    products = relationship("Product", secondary=purchase_products, back_populates="purchases", lazy="joined")
+
+    @hybrid_property
+    def count_products(self):
+        return len(self.products)
+
+    @count_products.expression
+    def count_products(cls):
+        return db.select([db.func.count(purchase_products.c.product_id)]).where(purchase_products.c.purchase_id == cls.id)
