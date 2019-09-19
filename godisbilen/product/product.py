6c2feabf754c5836ab977adfabd27e026add18d2
albinmedoc <stamline203@gmail.com>
2019-09-19 16:57:36 +0200

    Added stock and sold properties

--- godisbilen/product/product.py
+++ godisbilen/product/product.py
@@ -1,14 +1,25 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from godisbilen.app import db
-from godisbilen.purchase.purchase import purchase_products
+from godisbilen.purchase.purchase import PurchaseProducts
 
 class Product(db.Model):
     __tablename__ = "product"
     id = Column(Integer, primary_key=True)
     title = Column(String(100), unique=True)
-    stock = Column(Integer, unique=True)
-    purchases = relationship("Purchase", secondary=purchase_products, back_populates="products")
+    total = Column(Integer)
+    purchases = relationship("PurchaseProducts", back_populates="product")
+
+    @property
+    def stock(self):
+        return self.total - self.sold
+    
+    @property
+    def sold(self):
+        temp = 0
+        for purchase in self.purchases:
+            temp = temp + purchase.count
+        return temp
 
     def __repr__(self):
         return self.title
