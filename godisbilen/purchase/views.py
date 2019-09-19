d34cfc08a8dba32cd468289635719cf8a1f8e39b
albinmedoc <stamline203@gmail.com>
2019-09-19 17:08:36 +0200

    removed phone_number from column_searchable_list

--- godisbilen/purchase/views.py
+++ godisbilen/purchase/views.py
@@ -1,13 +1,9 @@
 from godisbilen.admin.views import AdminView
-from godisbilen.purchase import Purchase
-from godisbilen.user import User
 
 class PurchaseView(AdminView):
-    column_searchable_list = [User.phone_number]
+    column_searchable_list = ["order_number"]
     page_size = 50
     can_view_details = True
     create_modal = True
     edit_modal = True
-    column_hide_backrefs = False
-    column_select_related_list = (Purchase.user, Purchase.products)
-    column_list = ("id", "user", "count_products", "products")
+    column_list = ("order_number", "user", "count_products", "products")
