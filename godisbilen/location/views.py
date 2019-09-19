37f1e03513fc9b6a5139c6660f3b5ad683279fe3
albinmedoc <stamline203@gmail.com>
2019-09-19 16:58:22 +0200

    Added form_create_rules

--- godisbilen/location/views.py
+++ godisbilen/location/views.py
@@ -1,8 +1,9 @@
 from godisbilen.admin.views import AdminView
 
 class LocationView(AdminView):
-    page_size = 50
     column_searchable_list = ["city", "street", "postal_code", "lat", "lng"]
+    form_create_rules = ("city", "street", "street_number", "postal_code", "lat", "lng")
+    page_size = 50
     can_view_details = True
     create_modal = True
     edit_modal = True
