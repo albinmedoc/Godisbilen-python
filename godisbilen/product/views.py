035dd144a4434e3386c2ec48e3e263a33210b641
albinmedoc <stamline203@gmail.com>
2019-09-19 17:00:48 +0200

    Showing title, sold and stock. title and total for creation

--- godisbilen/product/views.py
+++ godisbilen/product/views.py
@@ -2,8 +2,9 @@ from godisbilen.admin.views import AdminView
 
 class ProductView(AdminView):
     column_searchable_list = ["title"]
+    form_create_rules = ("title", "total")
     page_size = 50
     can_view_details = True
     create_modal = True
     edit_modal = True
-    column_list = ("title", "stock")
+    column_list = ("title", "sold", "stock")
