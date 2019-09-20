3e4eef8acdaa31c309e435ee69c0f60a902abbbd
albinmedoc <stamline203@gmail.com>
2019-09-20 13:32:30 +0200

    Sending contact mail to ourselves

--- godisbilen/main/routes.py
+++ godisbilen/main/routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
+from flask_mail import Message
 from .utils import shop_open
 from .forms import ContactForm
+from godisbilen.app import mail
 
 bp_main = Blueprint("main", __name__)
 
@@ -20,6 +22,11 @@ def tos():
 def contact():
     form = ContactForm()
     if(form.validate_on_submit()):
+        msg = Message(subject=form.subject.data, recipients=[current_app.config["MAIL_DEFAULT_SENDER"]])
+        msg.body = "Meddelande från " + form.name.data + "(" + form.email.data + ")" + ". Innehåll: " + form.message.data
+        if(form.order_number.data.strip() != ""):
+            msg.body = msg.body + ". Ordernummer: " + form.order_number.data
+        mail.send(msg)
         return "Form validated"
     return render_template("contact.html", form=form)
 
