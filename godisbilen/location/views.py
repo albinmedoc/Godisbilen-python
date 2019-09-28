5efee2c599dafe13d4465825f1e270376a1eb026
albinmedoc <stamline203@gmail.com>
2019-09-29 00:03:49 +0200

    Removed because it caused crash

--- godisbilen/location/views.py
+++
@@ -1,9 +0,0 @@
-from godisbilen.admin.views import AdminView
-
-class LocationView(AdminView):
-    column_searchable_list = ["city", "street", "postal_code", "lat", "lng"]
-    form_create_rules = ("city", "street", "street_number", "postal_code", "lat", "lng")
-    page_size = 50
-    can_view_details = True
-    create_modal = True
-    edit_modal = True
