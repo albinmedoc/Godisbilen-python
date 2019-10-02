430d4f8c5dc05317eb4761531d00e2830e5be943
albinmedoc <stamline203@gmail.com>
2019-10-02 17:44:38 +0200

    Made stock and sold hybrid_properties

--- godisbilen/product/product.py
+++ godisbilen/product/product.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, select, func
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.hybrid import hybrid_property
 from godisbilen.app import db
 from godisbilen.purchase.purchase import PurchaseProducts
 
@@ -10,16 +11,24 @@ class Product(db.Model):
     total = Column(Integer)
     purchases = relationship("PurchaseProducts", back_populates="product")
 
-    @property
+    @hybrid_property
     def stock(self):
         return self.total - self.sold
     
-    @property
+    @stock.expression
+    def stock(cls):
+        return select([func.sum(PurchaseProducts.count) - cls.total]).where(PurchaseProducts.product_id==cls.id)
+    
+    @hybrid_property
     def sold(self):
         temp = 0
         for purchase in self.purchases:
             temp = temp + purchase.count
         return temp
+
+    @sold.expression
+    def sold(cls):
+        return select([func.sum(PurchaseProducts.count)]).where(PurchaseProducts.product_id==cls.id)
 
     def __repr__(self):
         return self.title
