b59d3cb7c4dce201be392dfbe6dabb5edab88cfd
albinmedoc <stamline203@gmail.com>
2019-10-02 17:44:59 +0200

    Added LocationView and gave all Views names

--- godisbilen/app.py
+++ godisbilen/app.py
@@ -46,13 +46,15 @@ def create_app(config_class=Config, create_db=False):
 
     from godisbilen.order.views import OrderView
     from godisbilen.user.views import UserView
+    from godisbilen.location.views import LocationView
     from godisbilen.product.views import ProductView
     from godisbilen.purchase.views import PurchaseView
 
-    admin.add_view(OrderView(Order, db.session, endpoint="orders"))
-    admin.add_view(UserView(User, db.session, endpoint="users"))
-    admin.add_view(ProductView(Product, db.session, endpoint="product"))
-    admin.add_view(PurchaseView(Purchase, db.session, endpoint="purchase"))
+    admin.add_view(OrderView(Order, db.session, endpoint="orders", name="Ordrar"))
+    admin.add_view(UserView(User, db.session, endpoint="users", name="Användare"))
+    admin.add_view(LocationView(Location, db.session, endpoint="location", name="Adresser"))
+    admin.add_view(ProductView(Product, db.session, endpoint="product", name="Produkter"))
+    admin.add_view(PurchaseView(Purchase, db.session, endpoint="purchase", name="Köp"))
     admin.init_app(app)
 
     return app
