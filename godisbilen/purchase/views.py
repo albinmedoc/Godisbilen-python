4a3b96943d307b30c215ecc97bffde7b2354bff8
albinmedoc <stamline203@gmail.com>
2019-10-02 17:47:13 +0200

    Added column_labels and made it only visible for Developers

--- godisbilen/purchase/views.py
+++ godisbilen/purchase/views.py
@@ -1,3 +1,4 @@
+from flask_login import current_user
 from godisbilen.admin.views import AdminView
 
 class PurchaseView(AdminView):
@@ -7,3 +8,13 @@ class PurchaseView(AdminView):
     create_modal = True
     edit_modal = True
     column_list = ("order_number", "user", "count_products", "products")
+    column_hide_backrefs = False
+    column_labels = {
+        "order_number": "Ordernummer",
+        "user": "Användare",
+        "count_products": "Antal produkter",
+        "products": "Produkter"
+    }
+
+    def is_visible(self):
+        return current_user.is_authenticated and current_user.has_roles("Developer")
