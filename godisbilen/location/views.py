c0d5642b79e8ce23387c7f810531eceff8716006
albinmedoc <stamline203@gmail.com>
2019-10-02 17:45:29 +0200

    Created LocationView (not searchable because of crash)

---
+++ godisbilen/location/views.py
@@ -0,0 +1,20 @@
+from flask_login import current_user
+from godisbilen.admin.views import AdminView
+
+class LocationView(AdminView):
+    column_searchable_list = []
+    page_size = 50
+    can_view_details = True
+    create_modal = False
+    column_list = ("id", "street_name", "street_number", "lat", "lng", "region")
+    column_labels = {
+        "street_name": "Gatunamn",
+        "street_number": "Gatunummer",
+        "lat": "Latitude",
+        "lng": "Longitude",
+        "region": "Region",
+        "products": "Produkter"
+    }
+
+    def is_visible(self):
+        return current_user.is_authenticated and current_user.has_roles("Admin", "Developer")
