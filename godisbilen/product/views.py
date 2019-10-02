3102cb3b72c8786ed179f45806ba012d14f41e5a
albinmedoc <stamline203@gmail.com>
2019-10-02 17:47:33 +0200

    Added column_labels and column_sortable_list

--- godisbilen/product/views.py
+++ godisbilen/product/views.py
@@ -2,9 +2,16 @@ from godisbilen.admin.views import AdminView
 
 class ProductView(AdminView):
     column_searchable_list = ["title"]
+    column_sortable_list = ("title", "sold", "stock", "total")
     form_create_rules = ("title", "total")
     page_size = 50
     can_view_details = True
     create_modal = True
     edit_modal = True
-    column_list = ("title", "sold", "stock")
+    column_list = ("title", "sold", "stock", "total")
+    column_labels = {
+        "title": "Namn",
+        "sold": "Sålda",
+        "stock": "Lager",
+        "total": "Totalt"
+    }
