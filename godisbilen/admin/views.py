211f31ef57a5072c47ffc318f4351ebd07dc0492
albinmedoc <stamline203@gmail.com>
2019-10-07 17:13:37 +0200

    Made so db pages redirects to login if not authenticated

--- godisbilen/admin/views.py
+++ godisbilen/admin/views.py
@@ -1,8 +1,12 @@
 from flask_admin.contrib.sqla import ModelView
-from flask import redirect, url_for
+from flask import redirect, url_for, request
 from flask_login import current_user
 
 
 class AdminView(ModelView):
     def is_accessible(self):
         return current_user.is_authenticated and current_user.has_roles("Admin")
+    
+    def inaccessible_callback(self, name, **kwargs):
+        # redirect to login page if user doesn't have access
+        return redirect(url_for("admin_route.login", next=request.url))
