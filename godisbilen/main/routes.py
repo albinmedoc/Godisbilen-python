036b3a32dff8edd4dfbf25b0502a898014117c6c
Albin Médoc <stamline203@gmail.com>
2019-11-07 16:24:54 +0100

    Route for campaign info and joining

--- godisbilen/main/routes.py
+++ godisbilen/main/routes.py
@@ -2,13 +2,39 @@ from flask import Blueprint, render_template, current_app, url_for
 from flask_mail import Message
 from .utils import shop_open
 from .forms import ContactForm
-from godisbilen.app import mail
+from godisbilen.app import mail, db
+from godisbilen.user import User
+from godisbilen.location import Location
+from godisbilen.campaign import Campaign, CampaignUsers
+from godisbilen.campaign.forms import JoinCampaignForm
 
 bp_main = Blueprint("main", __name__)
 
 @bp_main.route("/")
 def home():
     return render_template("main/home.html", shop_open=shop_open())
+
+@bp_main.route("/campaign/<int:campaign_id>", methods=["GET", "POST"])
+def campaign(campaign_id):
+    campaign = Campaign.query.filter_by(id=campaign_id).first()
+    if(not campaign):
+        return "Erbjudandet hittades inte"
+    form = JoinCampaignForm()
+    form.campaign_id.data = campaign_id
+    if(form.validate_on_submit()):
+        # Join campaign
+        user = User.query.filter_by(phone_number=form.phone_number.data).first()
+        if(not user):
+            user = User(phone_number=phone_number)
+            db.session.add(user)
+        location = Location.query.filter_by(lat=form.lat.data, lng=form.lng.data).first()
+        if(not location):
+            location = Location(lat=orm.lat.data, lng=form.lng.data)
+            db.session.add(location)
+        campaign.buyers.append(CampaignUsers(campaign_id=campaign_id, user=user, location=location))
+        db.session.commit()
+        return "Order lagd"
+    return render_template("main/campaign.html", campaign=campaign, form=form)
 
 @bp_main.route("/regions")   
 def regions():
