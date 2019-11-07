bc432b541a42759e62bd742b32c585a280e9e896
Albin Médoc <stamline203@gmail.com>
2019-11-07 16:20:43 +0100

    Added current year as prefix for order number

--- godisbilen/order/utils.py
+++ godisbilen/order/utils.py
@@ -1,9 +1,11 @@
 import string
 import random
+from datetime import datetime
 
 def random_order_number():
     from .order import Order
-    order_number = "".join(random.choice(string.ascii_uppercase) for x in range(20))
+    prefix = str(datetime.today().year) + "-"
+    order_number = prefix.join(random.choice(string.ascii_uppercase) for x in range(20))
     while(Order.query.filter_by(order_number=order_number).first()):
-        order_number = "".join(random.choice(string.ascii_uppercase) for x in range(20))
+        order_number = prefix.join(random.choice(string.ascii_uppercase) for x in range(20))
     return order_number
