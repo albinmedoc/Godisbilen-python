1f81af5f0a50fe22e720b379b446ded4186a16fd
Albin Médoc <stamline203@gmail.com>
2019-11-09 12:27:19 +0100

    Created a faq page

--- godisbilen/main/routes.py
+++ godisbilen/main/routes.py
@@ -59,3 +59,7 @@ def contact():
 @bp_main.route("/about")   
 def about():
     return render_template("main/about.html")
+
+@bp_main.route("/faq")   
+def faq():
+    return render_template("main/faq.html")
