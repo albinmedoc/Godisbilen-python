10d27a2cc974e10a67ac406a118fc54f6d31a0bd
Albin Médoc <stamline203@gmail.com>
2019-12-17 16:08:37 +0100

    Removed unnecesary print

--- godisbilen/region/routes.py
+++ godisbilen/region/routes.py
@@ -6,5 +6,4 @@ bp_region = Blueprint("regions", __name__)
 @bp_region.route("/get_region_boundaries", methods=["POST"])
 def get_region_bounds():
     active = request.values.get("active", default=True)
-    print(active)
     return jsonify(Region.get_bounds(lat_lng=True, active=active))
