d58f660d646b3d275b482c6a1d88786b46514cb2
Albin Médoc <stamline203@gmail.com>
2019-12-17 16:06:50 +0100

    Made so you can check for extra opening hours

--- godisbilen/main/utils.py
+++ godisbilen/main/utils.py
@@ -3,12 +3,23 @@ from flask import current_app
 
 def shop_open():
     shop_open = False
+
+    # Check standard opening hours
     dow = str(datetime.now().weekday())
     if(dow in current_app.config["OPENING_HOURS"]):
         for time_range in current_app.config["OPENING_HOURS"][dow]:
             if(is_time_between(time_range[0], time_range[1], datetime.now().time())):
                 shop_open = True
                 continue
+
+    # Check extra opening hours
+    current_date = datetime.now().date()
+    if(current_date in current_app.config["EXTRA_OPENING_HOURS"]):
+        for time_range in current_app.config["EXTRA_OPENING_HOURS"][current_date]:
+            if(is_time_between(time_range[0], time_range[1], datetime.now().time())):
+                shop_open = True
+                continue
+
     return shop_open
 
 def is_time_between(begin_time, end_time, check_time=None):
