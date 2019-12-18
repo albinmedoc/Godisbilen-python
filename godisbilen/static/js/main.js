8d4f404718ba5f7d2d7111f9ecd8bd46347f5650
Albin Médoc <stamline203@gmail.com>
2019-12-18 13:43:25 +0100

    Code to open/close navigation

--- godisbilen/static/js/main.js
+++ godisbilen/static/js/main.js
@@ -1,3 +1,15 @@
+document.getElementById("nav_btn").addEventListener("click", function(e){
+    var btn = document.getElementById("nav_btn");
+    var nav = document.getElementById("navigation")
+    if(btn.classList.contains("is-active")){
+        btn.classList.remove("is-active");
+        nav.classList.remove("is-active")
+    }else{
+        btn.classList.add("is-active");
+        nav.classList.add("is-active");
+    }
+});
+
 var getJSON = function (method, url, callback, params) {
     var xhr = new XMLHttpRequest();
     xhr.open(method, url, true);
