868b49aabd013eb38a1862c35e73171e4c1dd21a
Albin Médoc <stamline203@gmail.com>
2019-12-29 18:56:08 +0100

    Just adding eventlistener to nav if exists

--- godisbilen/static/js/main.js
+++ godisbilen/static/js/main.js
@@ -1,14 +1,16 @@
-document.getElementById("nav_btn").addEventListener("click", function(e){
-    var btn = document.getElementById("nav_btn");
-    var nav = document.getElementById("navigation")
-    if(btn.classList.contains("is-active")){
-        btn.classList.remove("is-active");
-        nav.classList.remove("is-active")
-    }else{
-        btn.classList.add("is-active");
-        nav.classList.add("is-active");
-    }
-});
+if(document.getElementById("nav_btn") &&  document.getElementById("navigation")){
+    document.getElementById("nav_btn").addEventListener("click", function(e){
+        var btn = document.getElementById("nav_btn");
+        var nav = document.getElementById("navigation");
+        if(btn.classList.contains("is-active")){
+            btn.classList.remove("is-active");
+            nav.classList.remove("is-active")
+        }else{
+            btn.classList.add("is-active");
+            nav.classList.add("is-active");
+        }
+    });
+}
 
 var getJSON = function (method, url, callback, params) {
     var xhr = new XMLHttpRequest();
