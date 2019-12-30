7602857a203e16709f061283b27dbd8bf92dfbe8
Albin Médoc <stamline203@gmail.com>
2019-12-30 13:51:58 +0100

    Added autofill location

--- godisbilen/static/js/address_picker.js
+++ godisbilen/static/js/address_picker.js
@@ -13,14 +13,28 @@ function initMap() {
     autocomplete.setFields(
         ["address_components", "geometry", "icon", "name"]);
 
-    autocomplete.addListener("place_changed", function () {
-        var place = autocomplete.getPlace();
+
+    // Create the infoWindow
+    var infowindow = new google.maps.InfoWindow();
+    var infowindowContent = document.getElementById("infowindow-content");
+    infowindow.setContent(infowindowContent);
+    infowindow.close();
+
+    // Create the marker
+    var marker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29)
+    });
+
+    function set_location(place){
+        // Set lat & lng to the place specified by the user
         document.getElementById("lat").value = place.geometry.location.lat();
         document.getElementById("lng").value = place.geometry.location.lng();
 
-        document.getElementById("map").parentElement.classList.add("active");
-        infowindow.close();
         marker.setVisible(false);
+
+        // Show the map
+        document.getElementById("map").parentElement.classList.add("active");
         if (!place.geometry) {
             alert("No details available for input: '" + place.name + "'");
             return;
@@ -31,22 +45,52 @@ function initMap() {
             map.setCenter(place.geometry.location);
         }
         map.setZoom(16);
+        
+        // Show the marker
         marker.setPosition(place.geometry.location);
         marker.setVisible(true);
 
-        infowindowContent.children["place-icon"].src = place.icon;
-        infowindowContent.children["place-name"].textContent = place.name;
+        // Set infoWindow content and show it
+        infowindowContent.children["place-name"].textContent = place.address_components[1].long_name + " " + place.address_components[0].short_name;
         if(place.address_components[6] && place.address_components[6].short_name && place.address_components[3] && place.address_components[3].short_name){
             infowindowContent.children["postal"].textContent = place.address_components[6].short_name + " " + place.address_components[3].short_name;
         }
         infowindow.open(map, marker);
+    }
+
+    autocomplete.addListener("place_changed", function () {
+        var place = autocomplete.getPlace();
+        set_location(place);
     });
 
-    var infowindow = new google.maps.InfoWindow();
-    var infowindowContent = document.getElementById("infowindow-content");
-    infowindow.setContent(infowindowContent);
-    var marker = new google.maps.Marker({
-        map: map,
-        anchorPoint: new google.maps.Point(0, -29)
-    });
+    var autofill = document.getElementById("autofill");
+    if(autofill){
+        // Add event listener
+        autofill.addEventListener("click", function(){
+            // Show the loader
+            showLoader(true);
+
+            // Check if browser supports geolocation
+            if(navigator.geolocation){
+
+                // Get the users location
+                navigator.geolocation.getCurrentPosition(function(position){
+                    var geocoder = new google.maps.Geocoder;
+                    var latlng = new google.maps.LatLng({lat: position.coords.latitude, lng: position.coords.longitude});
+                    // Get the place from the coordinates
+                    geocoder.geocode({'location': latlng}, function(results, status) {
+                        if (status === "OK" && results[0]) {
+                            // Show location
+                            set_location(results[0]);
+                        }
+                    });
+                });
+            }else{
+                alert("Geolocation is not supported by this browser.");
+            }
+
+            // Hide the loader
+            showLoader(false);
+        });
+    }
 }
