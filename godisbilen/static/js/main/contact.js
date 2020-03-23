424ac955b0412baccb8bcf6e1e0f8623523521db
Albin Médoc <stamline203@gmail.com>
2020-03-23 17:15:35 +0100

    form can be populated from get params. Autosizing message field

--- godisbilen/static/js/main/contact.js
+++ godisbilen/static/js/main/contact.js
@@ -1,14 +1,35 @@
-hide_order_number_form_row(true);
+// -- Loading form from URL --
+const urlParams = new URLSearchParams(window.location.search);
+var name = urlParams.get("name") || "";
+document.getElementById("name").value = name;
+var email = urlParams.get("email") || "";
+document.getElementById("email").value = email;
+var subject = urlParams.get("subject") || "";
+document.getElementById("subject").parentElement.childNodes[2].value = subject;
+var order_number = urlParams.get("order_number") || "";
+document.getElementById("order_number").value = order_number;
+var message = urlParams.get("message") || "";
+if(message != ""){
+    message = replaceAll(message, "%0A", "\n");
+}
+document.getElementById("message").value = message;
 
-document.getElementsByName("subject")[0].addEventListener("input", function(){
-    hide_order_number_form_row(this.value != "Angående order")
-});
 
-function hide_order_number_form_row(hide){
-    var order_number_form_row = document.getElementById("order_number").parentElement.parentElement;
-    if(hide){
-        order_number_form_row.style.display = "none";
+function show_hide_order_number(){
+    var order_number_input = document.getElementById("order_number").parentElement.parentElement;
+    var subject_value = document.getElementsByName("subject")[0].value;
+    if(subject_value !== "Angående order"){
+        order_number_input.style.display = "none";
     }else{
-        order_number_form_row.style.display = "flex";
+        order_number_input.style.display = "flex";
     }
 }
+
+// -- Hiding and showing order_number input -- 
+show_hide_order_number();
+document.getElementsByName("subject")[0].addEventListener("input", function(){
+    show_hide_order_number();
+});
+
+// Autosize message
+var autosize = new AutosizeTextarea(document.getElementById("message"));
